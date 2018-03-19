b6e0d3220b4146fd4fdc6b8bad59b9e49f0c1b07
Dan Ward <daniel.ward@forrent.com>
2018-03-19 18:39:04 -0400

    Add basic login call

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Route,
   Link
 } from 'react-router-dom'
+import axios from 'axios';
 
 import Dashboard from './components/Dashboard';
 import Help from './components/Help';
 import Messages from './components/Messages';
 import Subscribers from './components/Subscribers';
+import CREDS from './creds';
+import API from './api';
 
 import './App.css';
 
-const App = () => (
-    <div>
-      <ul>
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/messages">Messages</Link></li>
-        <li><Link to="/subscribers">Subscribers</Link></li>
-        <li><Link to="/help">Help</Link></li>
-      </ul>
+class App extends Component {
+  state = {
+    user: {}
+  }
 
-      <hr/>
+  componentDidMount() {
+    const token = window.btoa(window.unescape(window.encodeURIComponent(CREDS.string)));
 
-      <Route exact path="/" component={Dashboard}/>
-      <Route path="/help" component={Help}/>
-      <Route path="/messages" component={Messages}/>
-      <Route path="/subscribers" component={Subscribers}/>
-    </div>
-);
+    axios.get(API.findMe, {
+      headers: {
+        Authorization: `Basic ${token}`
+      }
+    }).then((user) => {
+      this.setState({
+        user
+       });
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/messages">Messages</Link></li>
+          <li><Link to="/subscribers">Subscribers</Link></li>
+          <li><Link to="/help">Help</Link></li>
+        </ul>
+  
+        <hr/>
+
+        <Route exact path="/" component={Dashboard}/>
+        <Route path="/help" component={Help}/>
+        <Route path="/messages" component={Messages}/>
+        <Route path="/subscribers" component={Subscribers}/>
+      </div>
+    );
+  }
+}
 
 export default App;
