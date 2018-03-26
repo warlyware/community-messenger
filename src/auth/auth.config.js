e0088af6fff6922e41cd457c134c5f1d56000f94
Dan Ward <daniel.ward@forrent.com>
2018-03-26 12:41:07 -0400

    Update auth config to redirect to last route when logging in

--- src/auth/auth.config.js
+++ src/auth/auth.config.js
@@ -1,4 +1,7 @@
-import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect'
+import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect';
+import locationHelperBuilder from 'redux-auth-wrapper/history4/locationHelper';
+
+const locationHelper = locationHelperBuilder({});
 
 export const UserIsAuthenticated = connectedRouterRedirect({
   // The url to redirect user to if they fail
@@ -12,7 +15,7 @@ export const UserIsAuthenticated = connectedRouterRedirect({
 
 export const UserIsNotAuthenticated = connectedRouterRedirect({
   // This sends the user either to the query param route if we have one, or to the landing page if none is specified and the user is already logged in
-  redirectPath: '/',
+  redirectPath: (state, ownProps) => locationHelper.getRedirectQueryParam(ownProps) || '/',
   // This prevents us from adding the query parameter when we send the user away from the login page
   allowRedirectBack: false,
    // If selector is true, wrapper will not redirect
