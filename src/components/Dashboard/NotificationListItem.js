0b4c7851d25221ee920de1371a848927e266466b
Dan Ward <daniel.ward@forrent.com>
2018-03-27 10:58:43 -0400

    Add momentjs, format data displayed in NotificationsListItem

--- src/components/Dashboard/NotificationListItem.js
+++ src/components/Dashboard/NotificationListItem.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import * as moment from 'moment';
+
+const actionTypes = {
+  SUBSCRIBE: 'SUBSCRIBE',
+  MESSAGE_INCOMING: 'MESSAGE_INCOMING',
+  UNSUBSCRIBE: 'UNSUBSCRIBE'
+}
 
 export default class NotificationListItem extends Component {
+  formatActionType(actionType) {
+    switch(actionType) {
+      case actionTypes.SUBSCRIBE:
+        return 'subscribed';
+      case actionTypes.MESSAGE_INCOMING:
+        return 'replied';
+      case actionTypes.UNSUBSCRIBE:
+        return 'unsubscribed';
+    }
+  }
+
   render() {
+    const name = this.props.notification.details.firstName || this.props.notification.details.sender.firstName;
+    const type = this.formatActionType(this.props.notification.type);
+
     return (
       <div className="row">
-        <h5 className="twelve columns margin-bottom-0">
-          {this.props.notification.details.firstName || this.props.notification.details.sender.firstName} has {this.props.notification.type}
-        </h5>
+        <h6 className="twelve columns margin-bottom-0 bold">
+          {name} has {type}.
+        </h6>
         <div className="twelve columns">
-          {this.props.notification.time}
+          {moment(this.props.notification.time).format('LLL')}
         </div>
       </div>
     );
