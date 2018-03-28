18ed30150c7727d70fe2c291f37c6fcd0fdc91cf
Dan Ward <daniel.ward@forrent.com>
2018-03-28 12:14:12 -0400

    Add live data to table in OutgoingMessageList

--- src/components/Messages/OutgoingMessageList/index.js
+++ src/components/Messages/OutgoingMessageList/index.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as moment from 'moment';
 
-export default class OutgoingMessageList extends Component {
+import { getMessages } from '../../../actions/messages';
+
+class OutgoingMessageList extends Component {
+  componentDidMount() {
+    this.props.getMessages()
+  }
+
   render() {
     return(
       <table>
-        <tr>
-          <th>Sent</th>
-          <th>Message</th>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Sent</th>
+            <th>Message</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {console.log(this.props.messages)}
+          {this.props.messages.map((message) => {
+            return(
+              <tr>
+                {console.log(message.sent)}
+                <td>{moment(message.sent).format('LLL')}</td>
+                <td>{message.title}</td>
+                <td>{message.status}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    messages: state.messages
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getMessages: () => dispatch(getMessages())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OutgoingMessageList);
